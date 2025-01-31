95ac1f8a6d4c260cb43fd2d5c5ca4c4f95d85118
cet-t <cets021@gmail.com>
2025-01-31 22:23:43 +0900

    first commit

---
+++ Analysis.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection.PortableExecutable;
+
+public class Analysis
+{
+    private static char[] chars = "1234567890()[]{}!?@#$%^&*(=-+_~`/\"';: \t\n™’‘”“".ToCharArray();
+
+    public static Task<(string, int)>? Read(string path)
+    {
+        if (NotExists(path))
+        {
+            Console.WriteLine("Invalid file path.");
+            return null;
+        }
+
+        return Task.Run(async () =>
+        {
+            int count = 0;
+            string text = string.Empty;
+            using var reader = new StreamReader(path);
+            while (!reader.EndOfStream)
+            {
+                var line = await reader.ReadLineAsync();
+                line = line?.Trim().ToLower();
+                foreach (var c in chars)
+                {
+                    line = line?.Replace(c.ToString(), string.Empty);
+                }
+                count += line?.Length ?? 0;
+                text += line;
+            }
+            return (text, count);
+        });
+    }
+
+    private static bool NotExists(string path)
+    {
+        return string.IsNullOrEmpty(path) || !File.Exists(path);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿var path = "eng";
+
+var _text = "";
+foreach (var file in Directory.GetFiles(path))
+{
+    var text = await Analysis.Read(file)!;
+    _text += text.Item1;
+    // File.WriteAllText(text.Item2 + ".." + Guid.NewGuid().ToString() + ".txt", text.Item1);
+}
+
+File.WriteAllText($"{_text.Length}.{Guid.NewGuid()}", _text);
