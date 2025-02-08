9e155438f514054e4a7e351c3cc4f58a0ec50088
cet-t <cets021@gmail.com>
2025-02-08 12:45:16 +0900

    analysis

--- Analysis.cs
+++ Analysis.cs
@@ -3,10 +3,11 @@ using System.Text.RegularExpressions;
 
 public class Analysis
 {
-    private static string pattern = "[^0-9a-z,.]"; // "\\W";
+    private static string pattern = "[^a-z,.]"; // "\\W";
     private static Regex re = new(pattern);
+    private static Dictionary<char, int> frequency = new();
 
-    public static Task<(string, int)>? Read(string path)
+    public static Task<(string, int, Dictionary<char, int>)>? Read(string path)
     {
         if (NotExists(path))
         {
@@ -25,9 +26,19 @@ public class Analysis
                 line = line?.Trim().ToLower();
                 line = re.Replace(line ?? string.Empty, string.Empty);
                 count += line?.Length ?? 0;
+                if (line?.Length > 0)
+                    foreach (var c in line)
+                    {
+                        if (!frequency.TryGetValue(c, out int value))
+                        {
+                            value = 0;
+                            frequency[c] = value;
+                        }
+                        frequency[c] = ++value;
+                    }
                 text += line;
             }
-            return (text, count);
+            return (text, count, frequency);
         });
     }
 
--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@ var _text = "";
 foreach (var file in Directory.GetFiles(path))
 {
     var text = await Analysis.Read(file)!;
+    File.WriteAllText(file.Split('\\')[^1] + "analysis.txt", string.Join("\n", text.Item3.OrderBy(x => x.Value).Select(x => $"{x.Key}: {x.Value}")));
     _text += text.Item1;
-    // File.WriteAllText(text.Item2 + ".." + Guid.NewGuid().ToString() + ".txt", text.Item1);
 }
 
 File.WriteAllText($"{_text.Length}.{Guid.NewGuid()}", _text);
